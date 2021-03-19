09d40e3c857e2a42cf70c6504a04121aad17bb42
kosimaru1997 <kosimaru1997@gmal.com>
2021-03-19 16:51:34 +0000

    [finish]new

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -2,10 +2,25 @@ class BookersController < ApplicationController
   def index
     @books = Book.all
   end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to("/")
+  end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.permit(:title,:body)
   end
 end
