bf77ff0fa099fa732175cbb3498f6232bebe8ccf
kosimaru1997 <kosimaru1997@gmal.com>
2021-03-22 07:16:56 +0000

    テストクリア、books_controllerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/' => "homes#top"
+  get '/', to: 'homes#top', as: 'root'
   
  resources :bookers
+ resources :books 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
